7f2c101d6f04d7235087cda9a38460909d16f18a
Bastien Duchesne <bastienduchesne@hotmail.fr>
2021-05-01 21:28:22 +0200

    liste des informations

--- ex_prix.py
+++ ex_prix.py
@@ -1,10 +1,41 @@
 import requests
 from bs4 import BeautifulSoup
 
-url='https://books.toscrape.com/index.html'
+
+
+url='https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
+
 r = requests.get(url)
 
 if r.ok:
     soup = BeautifulSoup(r.text, "html.parser")
-    genre = soup.findAll('href')
-print(len(genre))
+    product_page_url = url
+
+    """
+    price_including_tax = soup.find('tr' , {'id': 'incl'}).find('td')
+    price_excluding_tax = soup.fin('tr' , {'id': 'excl'}).find('td')
+    number_available = soup.fin('tr' , {'id': 'Availability'}).find('td')
+    """
+    trs = soup.findAll('tr')
+    title = soup.h1.text
+    description = soup.findAll('p')
+    rating = soup.find('p' , {'class': 'star-rating '})
+    soup.article.children
+  
+  
+    
+    
+    """
+    for tr in trs:
+        if tr.th.text == 'UPC':
+          print(tr.td.text)
+        if tr.th.text == 'Price (excl. tax)':
+          print(tr.td.text)
+        if tr.th.text == 'Price (incl. tax)':
+          print(tr.td.text)
+        if tr.th.text == 'Availability':
+          print(tr.td.text)
+"""
+
+
+   
